ac2fc55ed8a0cd18d47e756f6e869bde65a694cd
JasperMunene <jaspermunene2022@gmail.com>
2024-09-21 12:34:25 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    let left = dodger.style.left.replace("px", ""); 
+    let leftValue = parseInt(left, 10); 
+  
+    if (leftValue > 0) { 
+      dodger.style.left = `${leftValue - 10}px`; 
+    }
+  }
+  
+ 
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    let left = dodger.style.left.replace("px", ""); 
+    let leftValue = parseInt(left, 10); 
+  
+    if (leftValue < 360) { 
+      dodger.style.left = `${leftValue + 10}px`; 
+    }
+  }
+  
